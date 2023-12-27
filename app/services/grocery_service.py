eaa57fa4b19fd2f623f063d582747655b70b9344
Ian Whitesel <altocgamedev@gmail.com>
2023-12-27 12:06:28 -0500

    Delete function added

--- app/services/grocery_service.py
+++ app/services/grocery_service.py
@@ -3,38 +3,30 @@ from typing import Union
 from datetime import date
 import asyncio
 from databases import Database
-from database.database import connect_to_database, get_active_grocery_lists
+from database.database import connect_to_database, get_active_grocery_lists, deactivate_grocery_list
 from app.models.grocery_list import Base, GroceryList
 
 router = APIRouter()
 
 # Dependency to get the database session
-async def get_db():
-    loop = asyncio.get_event_loop()
-    return await loop.run_in_executor(None, connect_to_database)
+#async def get_db():
+#    loop = asyncio.get_event_loop()
+#    return await loop.run_in_executor(None, connect_to_database)
 
 # Route to retrieve active grocery lists
 @router.get("/list/active_grocery_lists")
-async def read_active_grocery_lists(db: Database = Depends(connect_to_database)):
+async def read_active_grocery_lists_endpoint(db: Database = Depends(connect_to_database)):
     active_grocery_lists = await get_active_grocery_lists(db)
     return {"active_grocery_lists": active_grocery_lists}
 
-@router.get("/list/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    """
-    Returns a grocery list for a given user.
-    If no user is supplied, returns all grocery lists.
-    """
-    grocery_list_data = {
-        "date": date.today(),
-        "name": "Example Grocery List",
-        "active": True
-    }
-    return GroceryList(**grocery_list_data)
-
-@router.put("/list/{item_id}")
-def update_grocery_list(item_id: int, grocery_list: GroceryList):
-    return {"grocery_list_name": grocery_list.name, "grocery_list_id": item_id}
+# Route to deactivate a grocery list
+@router.put("/list/deactivate/{list_id}")
+async def deactivate_grocery_list_endpoint(list_id: int, db: Database = Depends(connect_to_database)):
+    updated_grocery_list = await deactivate_grocery_list(db, list_id)
+    if updated_grocery_list:
+        return {"message": "Grocery list state updated successfully", "updated_grocery_list": updated_grocery_list}
+    else:
+        raise HTTPException(status_code=404, detail="Grocery list not found")
 
 @router.post("/list")
 def create_grocery_list(grocery_list: GroceryList):
--- database/database.py
+++ database/database.py
@@ -29,6 +29,20 @@ async def get_active_grocery_lists(db):
     result = await db.fetch_all(query)
     return result
 
+async def deactivate_grocery_list(db, list_id):
+    """
+    Sets a given list_id active_flag state to FALSE.
+    This effectively "deletes" the record from user perspective.
+    """
+    # Assuming you have a SQLAlchemy model named GroceryListDB
+    query = GroceryListDB.__table__.update().where(GroceryListDB.__table__.c.id == list_id).values(active_flag=False)
+    await db.execute(query)
+
+    # Optionally, you can return the updated record or a success message
+    updated_grocery_list = await db.fetch_one(GroceryListDB.__table__.select().where(GroceryListDB.__table__.c.id == list_id))
+    return updated_grocery_list
+
+
 async def disconnect_from_database(db):
     """
     Disconnects from the database.
